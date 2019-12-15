8a1bd17dd5cb40aa027edc36e9836dcb8a35bc28
Akash Verma <akashvermaofsjs@gmail.com>
2019-12-15 09:21:34 +0530

    Fixed TextRank

--- SentenceSelector.py
+++ SentenceSelector.py
@@ -7,12 +7,10 @@ import numpy as np
 def sentence_similarity(sent1, sent2, stop_words=None):
     if stop_words is None:
         stop_words = []
- 
+    
     sent1 = [w.lower() for w in sent1]
     sent2 = [w.lower() for w in sent2]
- 
     all_words = list(set(sent1 + sent2))
- 
     vector1 = [0] * len(all_words)
     vector2 = [0] * len(all_words)
  
@@ -27,45 +25,22 @@ def sentence_similarity(sent1, sent2, stop_words=None):
         if w in stop_words:
             continue
         vector2[all_words.index(w)] += 1
- 
     return 1 - cosine_distance(vector1, vector2)
-
-# Get a text from the Brown Corpus
-text="""Mahatma Gandhi is well known as the “Father of the Nation or Bapu” because of his greatest contributions towards the independence of our country. He was the one who believed in the non-violence and unity of the people and brought spirituality in the Indian politics. He worked hard for the removal of the untouchability in the Indian society, upliftment of the backward classes in India, raised voice to develop villages for social development, inspired Indian people to use swadeshi goods and other social issues. He brought common people in front to participate in the national movement and inspired them to fight for their true freedom.
-
-He was one of the persons who converted people’s dream of independence into truth a day through his noble ideals and supreme sacrifices. He is still remembered between us for his great works and major virtues such as non-violence, truth, love and fraternity. He was not born as great but he made himself great through his hard struggles and works. He was highly influenced by the life of the King Harischandra from the play titled as Raja Harischandra. After his schooling, he completed his law degree from England and began his career as a lawyer. He faced many difficulties in his life but continued walking as a great leader.
-
-He started many mass movements like Non-cooperation movement in 1920, civil disobedience movement in 1930 and finally the Quit India Movement in 1942 all through the way of independence of India. After lots of struggles and works, independence of India was granted finally by the British Government. He was a very simple person who worked to remove the colour barrier and caste barrier. He also worked hard for removing the untouchability in the Indian society and named untouchables as “Harijan” means the people of God.
-
-He was a great social reformer and Indian freedom fighter who died a day after completing his aim of life. He inspired Indian people for the manual labour and said that arrange all the resource ownself for living a simple life and becoming self-dependent. He started weaving cotton clothes through the use of Charakha in order to avoid the use of videshi goods and promote the use of Swadeshi goods among Indians.
-
-He was a strong supporter of the agriculture and motivated people to do agriculture works. He was a spiritual man who brought spirituality to the Indian politics. He died in 1948 on 30th of January and his body was cremated at Raj Ghat, New Delhi. 30th of January is celebrated every year as the Martyr Day in India in order to pay homage to him.
-
-"""
-
-#print(sentences)
- 
-#print(len(sentences))  #  98
- 
-
- 
  
 def build_similarity_matrix(sentences, stop_words=None):
     # Create an empty similarity matrix
     S = np.zeros((len(sentences), len(sentences)))
- 
- 
     for idx1 in range(len(sentences)):
         for idx2 in range(len(sentences)):
             if idx1 == idx2:
                 continue
- 
-            S[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
- 
+            S[idx1][idx2] = sentence_similarity(sentences[idx1].split(), sentences[idx2].split(), stop_words)
+    #print(S)
     # normalize the matrix row-wise
     for idx in range(len(S)):
-        S[idx] /= S[idx].sum()
- 
+        sum=S[idx].sum()
+        if(sum!=0):
+            S[idx] /= S[idx].sum()
     return S
 
 def pagerank(A, eps=0.0001, d=0.85):
@@ -100,3 +75,5 @@ def select_sentences(text,num):
     # get the english list of stopwords
     stop_words = stopwords.words('english')
     return textrank(sentences,num, stop_words)
+
+#print(select_sentences("I want to go to the good market. This is a bad sentence. This is a good sentence.",2))
--- main.py
+++ main.py
@@ -4,30 +4,33 @@ from nltk import Tree
 import nltk
 from textblob import TextBlob
 
-text="""World War II thus far, has been the deadliest and bloodiest war to date. More than 38 million people died by the end of the war, many of them innocent civilians. It was also the most destructive war in our current history. The fighting raged on in many parts of the world, with the brunt of it being in Europe and Japan. More than 50 nations took part in this war, which changed the world forever. For Americans, World War II had a clear-cut purpose; they were fighting to defeat tyranny. Most of Europe had been conquered by Nazi Germany, which was under the evil control of Adolf Hitler. The war in Europe began with Germany’s unprecedented invasion of Poland in 1939. It seemed that wherever the Nazi army went, they came down with a vengeance on the Jews of that area. They also went after anyone that didnt fit in to their idea of the Master Race, Aryans."""
+text="""An old man lived with his four sons in a village. The old man was worried. His sons were always quarrelling with each other. He had tried telling them many times to avoid fighting. But his sons would not listen to him.
+One day, he called his four sons. He gave them a small bundle of sticks, and asked them to break the bundle into two. The bundle was made of four sticks. “It’s child’s play,” said the eldest son. He took the bundle and tried to break it. He was surprised that the sticks in the bundle remained intact. He used more force. He tried again and again. He started panting for breath. The bundle would not break. He gave up.Then his brothers tried to break the bundle of four sticks without success.Their father smiled and asked them to untie the bundle. He asked each brother to take one stick and try to break it. Each of the sons took a stick in hand. In no time, the sticks were bent and broken.
+“A single stick is easily broken. If four sticks come together it is impossible to break them,” said the old man, giving his sons a meaningful look.
+This time the lesson went home. The brothers stopped fighting each other. They would work together as a team and succeed in doing whatever work was given to them.
+The four boys had discovered that unity is strength."""
 
 summary=select_sentences(text,10)
 
-NP = "NP: { <NNP>+|<CD> }"
-#NP = "NP: { <JJ><NN>|<JJ><NNP>|<NN><NN>|<NN>|<NNP><NNP>|<NNP>|<CD> }"
+#NP = "NP: { <NN>+|<CD> }"
+NP = "NP: { <JJ><NN>|<JJ><NNP>|<NN><NN>|<NN>|<NNP><NNP>|<NNP>|<CD> }"
 chunker = nltk.RegexpParser(NP)
-
+#print(summary)
 for i in summary:
-    #print(i)
     result = chunker.parse(pos_tag(word_tokenize(i)))
     list_NP=[]
+    #print(result)
     for subtree in result.subtrees(filter=lambda t: t.label() == 'NP'):
         str=""
         for temp in subtree.leaves():
-            if(str!=""):
+            if(str!=""):#Adding spaces between lines if not first word.
                 str+=" "
             str+=temp[0]
         list_NP.append(str)
-
     #print(list_NP)
-    
     for gaps in list_NP:
         q=i.replace(gaps,"_______")
+        print("Original Sentence: {}".format(i))
         print("Question: {}".format(q))
         print("Answer: {}".format(gaps))
         print()
